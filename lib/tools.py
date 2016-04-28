2187f2ec3786ab1d6260009b686fff749bd445fa
yubang <yubang93@gmail.com>
2016-04-28 23:09:11 +0800

    增加抓取电子工程学院信息

---
+++ lib/tools.py
@@ -0,0 +1,39 @@
+# coding:UTF-8
+
+
+"""
+常用工具类
+
+"""
+
+
+import re
+
+
+def get_company_name(html):
+    """
+    提取公司名字
+    :param html: 一段字符串
+    :return:
+    """
+    return "未识别的公司"
+
+
+def get_work_citys(html):
+    """
+    获取工作城市
+    :param html: 一段字符串
+    :return:
+    """
+    return []
+
+
+def get_work_position(html):
+    """
+    获取职位
+    :param html: 一段字符串
+    :return:
+    """
+    return []
+
+print('ok')
