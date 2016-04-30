5f424173e73201805ec00158fa8877136e158f45
yubang <yubang93@gmail.com>
2016-04-30 15:40:22 +0800

    修改数据库配置

---
+++ config/db_config.py
@@ -0,0 +1,29 @@
+# coding:UTF-8
+
+
+"""
+数据库配置
+"""
+
+
+import os
+
+
+db_config_data = {
+    'db_name': 'work',
+    'db_user': 'root',
+    'db_password': 'root',
+    'db_port': 3306,
+    'db_host': '127.0.0.1'
+}
+
+if os.environ.get('own_env_name', None) == 'yubang':
+    db_config_data = {
+        'db_name': 'work',
+        'db_user': 'root',
+        'db_password': 'root',
+        'db_port': 3306,
+        'db_host': '127.0.0.1'
+    }
+
+
