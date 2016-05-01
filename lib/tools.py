c3904eafaa718e2d62e6595ccf0656d44638bc1b
wususu <keven_susu@126.com>
2016-05-02 00:19:37 +0800

    更改

--- lib/tools.py
+++ lib/tools.py
@@ -8,7 +8,11 @@
 
 
 from bs4 import BeautifulSoup
+from config.citys import citys
+from config.position import positions
 import re
+import time
+import random
 
 
 def handle_company_name_use_black_data(objs, index):
@@ -18,7 +22,9 @@ def handle_company_name_use_black_data(objs, index):
     :return:
     """
     for obj in objs:
-        if re.search(r'([随着]|[成立])+.*?([公司]|[企业]|[集团]|[公司]|[研发中心])+', obj[index]):
+        if re.search(r'([随着]|[成立]|[负责])+.*?([公司]|[企业]|[集团]|[公司]|[研发中心]|[推进中心]|[传媒])+', obj[index]):
+            continue
+        if re.search(r'\.[\s ]*公司', obj[index]):
             continue
         return obj[index]
 
@@ -32,22 +38,31 @@ def get_company_name(html):
     :return:
     """
     html = BeautifulSoup(html, "html.parser").get_text()
-    r = re.findall(r'(.*\d+年{0,1}的)?(.*[：，,。]+)?(.*?有限公司)', html)
+    r = re.findall(r'(.*\d+年{0,1}的)?(.*[：，,。]+)?(.*隶?属于)?(.*\.\s+)?(.+?有限公司)', html)
 
+    if r:
+        return handle_company_name_use_black_data(r, 4)
+
+    r = re.findall(r'(.*\d+年?的)?(.*[：，,。]+)?(.+?研发中心)', html)
     if r:
         return handle_company_name_use_black_data(r, 2)
 
-    r = re.findall(r'(.*\d+年?的)?(.*[：，,。]+)?(.*?研发中心)', html)
+    r = re.findall(r'(.*\d+年?的)?(.*[：，,。]+)?是?(.+?集团)', html)
     if r:
         return handle_company_name_use_black_data(r, 2)
 
-    r = re.findall(r'(.*\d+年?的)?(.*[：，,。]+)?是?(.*?集团)', html)
+    r = re.findall(r'(.*\d+年?的)?(.*[：，,。]+)?(.*\.\s+)?(.+?公司)', html)
+    if r:
+        return handle_company_name_use_black_data(r, 3)
+
+    r = re.findall(r'(.*\d+年?的)?(.*[：，,。]+)?(.+?推进中心)', html)
     if r:
         return handle_company_name_use_black_data(r, 2)
 
-    r = re.findall(r'(.*\d+年?的)?(.*[：，,。]+)?(.*?公司)', html)
+    r = re.findall(r'(.*\d+年?的)?(.*[：，,。]+)?(.+?传媒)', html)
     if r:
         return handle_company_name_use_black_data(r, 2)
+
 
     return "未识别的公司"
 
@@ -59,10 +74,16 @@ def get_work_citys(html):
     :return:
     """
     html = BeautifulSoup(html, "html.parser").get_text()
-    r = re.findall(r'(工作地[点]?[:：]?){1}(可选：)?(.*)', html)
-    if r and r[0][2]:
-        return re.split(r'[#,、，\s]', r[0][2])
-    return []
+    # r = re.findall(r'(工作地[点]?[:：]?){1}(可选：)?(.*)', html)
+    # if r and r[0][2]:
+    #     return re.split(r'[#,、，\s]', r[0][2])
+    # return []
+
+    r = []
+    for city in citys:
+        if html.find(city) != -1:
+            r.append(city)
+    return r
 
 
 def get_work_position(html):
@@ -71,8 +92,48 @@ def get_work_position(html):
     :param html: 一段字符串
     :return:
     """
-    return []
+    html = BeautifulSoup(html, "html.parser").get_text()
+    r = []
+    for p in positions:
+        if html.find(p) != -1:
+            r.append(p)
+    return r
 
+
+def get_real_time(time_str):
+    """
+    获取标准的时间
+    :param time_str:
+    :return:
+    """
+    time_str = time_str.replace('.', '-')
+    time_str = time_str.replace('年', '-')
+    time_str = time_str.replace('月', '-')
+    time_str = time_str.replace('日', '')
+    time_str = time_str.replace('时', '：')
+    time_str = time_str.replace('分', '：')
+    time_str = time_str.replace('秒', '')
+
+    attrs = time_str.split(' ')
+    t = attrs[0]
+    if not t:
+        t = attrs[1]
+
+    arrs = t.split("-")
+    if len(arrs) != 3:
+        t = time.strftime("%Y-") + t
+
+    return t + " 00:00:00"
+
+
+def sleep_some_time():
+    """
+    随机休眠几秒
+    :return:
+    """
+    t = random.random() / 2 * 10
+    print("休眠" + str(t) + "秒")
+    time.sleep(t)
 
 # with open('../debug_html/2.html', 'r') as fp:
 #     print(get_work_citys(fp.read()))
