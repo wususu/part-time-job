007b5f67fd5af1c112a621d85233d065df28dfef
wususu <keven_susu@126.com>
2016-05-02 00:26:52 +0800

    更改

--- config/db_config.py
+++ config/db_config.py
@@ -17,11 +17,11 @@ db_config_data = {
     'db_host': '127.0.0.1'
 }
 
-if os.environ.get('own_env_name', None) == 'yubang':
+if os.environ.get('own_env_name', None) == 'wususu':
     db_config_data = {
         'db_name': 'work',
         'db_user': 'root',
-        'db_password': '',
+        'db_password': 'root',
         'db_port': 3306,
         'db_host': '127.0.0.1'
     }
