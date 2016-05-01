29b2618a908fdc2c8a7c1d010d5f192946fa9f5c
wususu <keven_susu@126.com>
2016-05-02 00:17:14 +0800

    更改

--- fetch/scau_gongcheng.py
+++ fetch/scau_gongcheng.py
@@ -2,9 +2,10 @@
 import re
 import requests
 from bs4 import BeautifulSoup
-from part_time_and_work.fetch import tools
+from lib import tools,dao
+from lib.logging_lib import log
 
-final_data = []
+
 zhaopin_data = {}
 url_first = 'http://gcxy.scau.edu.cn/Channel-transfer-cid-13' #入口
 page_url = 'http://gcxy.scau.edu.cn/Channel-transfer-cid-13-p-'
@@ -13,6 +14,9 @@ data_source_url = 'http://gcxy.scau.edu.cn'
 
 def Get_html(url):
     req = requests.get(url)
+    if req.status_code != 200:
+        log.error("网址（%s）无法访问，状态码：%d" % (url, req.status_code))
+        return None
     html=req.content
     return html
 
@@ -50,14 +54,16 @@ def get_job_url(url):
 
 
 def get_title(html):
+    html = BeautifulSoup(html,"html.parser")
     try:
         title = html.find("h2",{"class":"article-content-title"}).text
         return title
     except:
         return '提取失败'
 
+
 def get_date(html):
-    html_world = html.get_text()
+    html_world = BeautifulSoup(html, "html.parser").get_text()
     try:
         x = re.findall(r'作者：(.+)时间：(\d\d\d\d-\d{1,2}-\d{1,2}).{2}来源',html_world)
         date = x[0][1]
@@ -66,36 +72,45 @@ def get_date(html):
         return '提取失败'
 
 
-def get_all_data():
+def handle_all_data(url):
     """
-    采集工作信息
+    获取每一条信息
+    :param url:
     :return:
     """
-    #x=1
+    tools.sleep_some_time()
+    zhaopin_data = {}
+    html = Get_html(url)
+    zhaopin_data['web_url'] = url
+    zhaopin_data['web_html'] = html
+    zhaopin_data['title'] = get_title(html)
+    zhaopin_data['release_time'] = tools.get_real_time(get_date(html))
+    zhaopin_data['company'] = tools.get_company_name(html)
+    zhaopin_data['position'] = tools.get_work_position(html)
+    zhaopin_data['work_city'] = tools.get_work_citys(html)
+    zhaopin_data['message_source'] = '华农工程学院官网'
+    print(zhaopin_data['title'],zhaopin_data['release_time'],zhaopin_data['message_source'],zhaopin_data['company'])
+    return zhaopin_data
+
+
+def get_all_data():
+    final_data = []
     html = Get_html(url_first)
     all_url_list = get_pages(html)
     for url in all_url_list:
-        html_1 = Get_html(url)
-        html = BeautifulSoup(html_1,"html.parser")
-        zhaopin_data['web_url'] = url
-        zhaopin_data['web_html'] = html_1
-        zhaopin_data['title'] = get_title(html)
-        zhaopin_data['release_time'] = get_date(html)
-        zhaopin_data['company'] = tools.get_company_name(html_1)
-        zhaopin_data['position'] = tools.get_work_position(html_1)
-        zhaopin_data['work_city'] = tools.get_work_citys(html_1)
-        zhaopin_data['message_source'] = ''
-        zhaopin_data['job_type'] = ''
-        #打印所有信息
-        #print("招聘",x,zhaopin_data['title'],zhaopin_data['release_time'],zhaopin_data['web_url'],zhaopin_data['company'])
-        #x+=1
-        final_data.append(zhaopin_data)
+        final_data.append(handle_all_data(url))
     return final_data
 
 
 def text():
-    get_all_data()
-    print(len(final_data))
+
+    print(len(get_all_data()))
+
+def init():
+    objs = get_all_data()
+    for obj in objs:
+        dao.add_a_job(obj['title'], obj['company'], obj['web_url'], obj['work_city'], obj['message_source'], obj['position'],
+                      obj['release_time'], obj['web_html'])
 
 if __name__ == '__main__':
     text()
